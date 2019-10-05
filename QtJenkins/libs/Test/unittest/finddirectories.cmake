e5bb8f3b090912a483be78ee7d657423218a5931
amm1sc <mahmoud.abdelazeem@heidolph.de>
2019-10-06 00:34:20 +0200

    add the file

--- QtJenkins/libs/Test/unittest/finddirectories.cmake
+++ QtJenkins/libs/Test/unittest/finddirectories.cmake
@@ -1,7 +1,7 @@
 cmake_minimum_required(VERSION 3.10.2)
 
     include_directories(~/Projects/qtpipeline/QtJenkins/libs/Benchtop)
-    include_directories(~/workspace/qtpipeline/QtJenkins/libs/Benchtop)
+   # include_directories(~/workspace/qtpipeline/QtJenkins/libs/Benchtop)
 
         include_directories($WORKSPACE/qtpipeline/QtJenkins/libs/Benchtop)
 
